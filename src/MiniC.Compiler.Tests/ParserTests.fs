3387af792cce28b17a5cd5a4ca6dca39befa82d4
Tim Jones <tim@timjones.tw>
2013-01-17 22:56:17 +0800

    Add failing test for binary expression

--- src/MiniC.Compiler.Tests/ParserTests.fs
+++ src/MiniC.Compiler.Tests/ParserTests.fs
@@ -19,7 +19,20 @@ let ``can parse empty main method with random whitespace``() =
 
 [<Test>]
 let ``can parse nearly-empty main method``() =
-    let result = Parser.parse "void main(void) { ; }"
+    let result = Parser.parse "
+        void main(void) {
+            ;
+        }"
+    let expected =
+        [Ast.FunctionDeclaration(Ast.Void, "main", None, (None, [Ast.ExpressionStatement(Ast.Nop)])) ]
+    Assert.That(result, Is.EqualTo(expected))
+
+[<Test>]
+let ``can parse binary expression``() =
+    let result = Parser.parse "
+        void main(void) {
+            123+456
+        }"
     let expected =
         [Ast.FunctionDeclaration(Ast.Void, "main", None, (None, [Ast.ExpressionStatement(Ast.Nop)])) ]
     Assert.That(result, Is.EqualTo(expected))
