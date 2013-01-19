505588dbc9cbec67d9bbdc35ae70dbc8912783da
Tim Jones <tim@timjones.tw>
2013-01-19 18:58:17 +0800

    Implement basic .exe generation

---
+++ src/MiniC.Compiler.Tests/CompilerTests.fs
@@ -0,0 +1,16 @@
+﻿module MiniC.Compiler.Tests.CompilerTests
+
+open System.Reflection
+open NUnit.Framework
+open MiniC.Compiler
+
+[<Test>]
+let ``can generate .NET assembly``() =
+    let parseTree = [Ast.FunctionDeclaration(Ast.Void, "main", None, (None, [])) ]
+    let (compiledType, entryPoint) = Compiler.compileToMemory (new AssemblyName "Foo") parseTree
+    
+    Assert.That(compiledType, Is.Not.Null)
+    Assert.That(entryPoint, Is.Not.Null)
+
+    // Use reflection to execute.
+    entryPoint.Invoke(None, Array.empty) |> ignore
---
+++ src/MiniC.Compiler/Compiler.fs
@@ -0,0 +1,23 @@
+﻿module MiniC.Compiler.Compiler
+
+open System
+open System.Reflection
+open System.Reflection.Emit
+open Ast
+
+let compile (typeBuilder : TypeBuilder) code =
+    let methodBuilder = typeBuilder.DefineMethod("Main", MethodAttributes.Static ||| MethodAttributes.Public)
+    let ilGenerator = methodBuilder.GetILGenerator()
+    ilGenerator.Emit OpCodes.Ret
+    methodBuilder
+
+let compileToMemory assemblyName code =
+    let a = AppDomain.CurrentDomain.DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess.RunAndSave)
+    let m = a.DefineDynamicModule (assemblyName.Name, assemblyName.Name + ".exe")
+    let typeBuilder = m.DefineType(assemblyName.Name + ".Program", TypeAttributes.Abstract ||| TypeAttributes.Sealed ||| TypeAttributes.Public)
+    
+    let entryPoint = compile typeBuilder code
+    a.SetEntryPoint(entryPoint)
+
+    let compiledType = typeBuilder.CreateType()
+    (compiledType, compiledType.GetMethod(entryPoint.Name))
